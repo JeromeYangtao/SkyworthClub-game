41b01cc8ef928f56c75296cd1c0782315316be59
JeromeYangtao <779387850@qq.com>
2017-09-19 23:53:27 +0800

    Update index.js

--- runningMan/index.js
+++ runningMan/index.js
@@ -45,30 +45,32 @@ function dp_submitScore (m, t) {
   // return false
 }
 
-document.addEventListener('WeixinJSBridgeReady', function onBridgeReady () {
-  WeixinJSBridge.on('menu:share:appmessage', function (argv) {
-    WeixinJSBridge.invoke('sendAppMessage', {
-      'img_url': window.shareData.imgUrl,
-      'link': window.shareData.timeLineLink,
-      'desc': window.shareData.tContent,
-      'title': window.shareData.tTitle
-    }, function (res) {
-      document.location.href = mebtnopenurl
-    })
-  })
-  WeixinJSBridge.on('menu:share:timeline', function (argv) {
-    WeixinJSBridge.invoke('shareTimeline', {
-      'img_url': window.shareData.imgUrl,
-      'img_width': '640',
-      'img_height': '640',
-      'link': window.shareData.timeLineLink,
-      'desc': window.shareData.tContent,
-      'title': window.shareData.tTitle
-    }, function (res) {
-      document.location.href = mebtnopenurl
-    })
-  })
-}, false)
+
+wx.onMenuShareTimeline({
+  title: window.shareData.tTitle, // 分享标题
+  link: window.shareData.link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+  imgUrl: window.shareData.imgurl, // 分享图标
+  success: function () {
+    // 用户确认分享后执行的回调函数
+  },
+  cancel: function () {
+    // 用户取消分享后执行的回调函数
+  }
+})
+wx.onMenuShareAppMessage({
+  title: window.shareData.tTitle, // 分享标题
+  desc: window.shareData.tContent, // 分享描述
+  link: window.shareData.timeLineLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+  imgUrl: window.shareData.imgurl, // 分享图标
+  type: 'link', // 分享类型,music、video或link，不填默认为link
+  dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+  success: function () {
+    // 用户确认分享后执行的回调函数
+  },
+  cancel: function () {
+    // 用户取消分享后执行的回调函数
+  }
+})
 
 document.querySelector('#share').addEventListener('click', function () {
   this.style.display = 'none'
